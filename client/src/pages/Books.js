185ccef649f2063453d3f35c082e6b6f433bc227
kr4mpu5 <derkrampus@icloud.com>
2020-04-07 16:21:10 -0500

    implemented google books search functionality

--- client/src/pages/Books.js
+++ client/src/pages/Books.js
@@ -1,155 +1,128 @@
-import React, { useState, useEffect } from "react";
-import DeleteBtn from "../components/DeleteBtn";
+// import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
+// import DeleteBtn from "../components/DeleteBtn";
 import Jumbotron from "../components/Jumbotron";
-import API from "../utils/API";
+// import API from "../utils/API";
 import GBSearch from "../utils/GBSearch";
 import { Link } from "react-router-dom";
 import { Col, Row, Container } from "../components/Grid";
 import { List, ListItem } from "../components/List";
-import { Input, TextArea, FormBtn } from "../components/Form";
+// import { Input, TextArea, FormBtn } from "../components/Form";
+import { Input, FormBtn } from "../components/Form";
 
-function Books() {
-  // Setting our component's initial state
-  const [books, setBooks] = useState([])
-  const [gbFormObject, setGBFormObject] = useState({})
-  const [formObject, setFormObject] = useState({})
+export default function Books() {
+  const [book, setBook] = useState('');
+  const [results, setResults] = useState([]);
+  const [gbFormObject, setGBFormObject] = useState({});
 
-  // Load all books and store them with setBooks
-  useEffect(() => {
-    loadBooks()
-  }, [])
-
-  // Loads all books and sets them to books
-  function loadBooks() {
-    API.getBooks()
-      .then(res =>
-        setBooks(res.data)
-      )
-      .catch(err => console.log(err));
-  };
-
-  // Deletes a book from the database with a given id, then reloads books from the db
-  function deleteBook(id) {
-    API.deleteBook(id)
-      .then(res => loadBooks())
-      .catch(err => console.log(err));
-  }
-
-  function handleGBSearchInputChange(event) {
-    const { name, value } = event.target;
-    setGBFormObject({ ...gbFormObject, [name]: value })
-  };
-
-  // Handles updating component state when the user types into the input field
   function handleInputChange(event) {
-    const { name, value } = event.target;
-    setFormObject({ ...formObject, [name]: value })
+    setBook(event.target.value);
   };
 
-  function handleGBSearchFormSubmit(event) {
-    event.preventDefault();
-    if (gbFormObject.gbSearchTitle) {
-      console.log(gbFormObject.gbSearchTitle);
-      GBSearch.findBooks({
-        q: gbFormObject.gbSearchTitle,
-      })
-        .then(res => loadBooks())
-        .catch(err => console.log(err));
-    }
-  };
-
-
-  // When the form is submitted, use the API.saveBook method to save the book data
-  // Then reload books from the database
   function handleFormSubmit(event) {
     event.preventDefault();
-    if (formObject.title && formObject.author) {
-      API.saveBook({
-        title: formObject.title,
-        author: formObject.author,
-        synopsis: formObject.synopsis
-      })
-        .then(res => loadBooks())
+    if (book) {
+      GBSearch.findBooks(book)
+        // .then(res => console.log(res.data))
+        .then(res => {
+          console.log(res.data.items);
+          setResults(res.data.items);
+        })
         .catch(err => console.log(err));
     }
   };
+
+  function handleViewSubmit(event) {
+    event.preventDefault();
+    alert('Not yet implemented.');
+  }
+
+  function handleSaveSubmit(event) {
+    event.preventDefault();
+    alert('Not yet implemented.');
+  }
 
   return (
     <Container fluid>
+      <Row><Col size='md-12'><div>&nbsp;</div></Col></Row>
       <Row>
         <Col size="md-12">
           <Jumbotron>
-            <h1> Google Books Search</h1>
-            <h3>Search for and Save Books of Interest</h3>
+            <h1>Google Books Search</h1>
+            <p>Search for and Save Books of Interest</p>
           </Jumbotron>
-          <form>
-            <Input
-              onChange={handleGBSearchInputChange}
-              name="gbSearchTitle"
-              placeholder="What book are you looking for?"
-            />
-            <FormBtn
-            onClick={handleGBSearchFormSubmit}
-            >
-              Search
-            </FormBtn>
-          </form>
         </Col>
       </Row>
       <Row>
-        <Col size="md-6">
-          <Jumbotron>
-            <h1>What Books Should I Read?</h1>
-          </Jumbotron>
-          <form>
-            <Input
-              onChange={handleInputChange}
-              name="title"
-              placeholder="Title (required)"
-            />
-            <Input
-              onChange={handleInputChange}
-              name="author"
-              placeholder="Author (required)"
-            />
-            <TextArea
-              onChange={handleInputChange}
-              name="synopsis"
-              placeholder="Synopsis (Optional)"
-            />
-            <FormBtn
-              disabled={!(formObject.author && formObject.title)}
-              onClick={handleFormSubmit}
-            >
-              Submit Book
-              </FormBtn>
-          </form>
-        </Col>
-        <Col size="md-6 sm-12">
-          <Jumbotron>
-            <h1>Books On My List</h1>
-          </Jumbotron>
-          {books.length ? (
-            <List>
-              {books.map(book => (
-                <ListItem key={book._id}>
-                  <Link to={"/books/" + book._id}>
-                    <strong>
-                      {book.title} by {book.author}
-                    </strong>
-                  </Link>
-                  <DeleteBtn onClick={() => deleteBook(book._id)} />
-                </ListItem>
-              ))}
-            </List>
-          ) : (
-              <h3>No Results to Display</h3>
-            )}
+        <Col size="md-12">
+          <div className="card">
+            <div className="card-body">
+              <h3>Search</h3>
+              <form>
+                <Input
+                  onChange={handleInputChange}
+                  name="gbSearchTitle"
+                  placeholder="What book are you looking for?"
+                />
+                <FormBtn onClick={handleFormSubmit}>
+                  Search
+                </FormBtn>
+              </form>
+            </div>
+          </div>
         </Col>
       </Row>
+      <Row><Col size='md-12'><div>&nbsp;</div></Col></Row>
+      <Row>
+        <Col size="md-12">
+          <div className="card">
+            <div className="card-body">
+              <h3>Results</h3>
+              <div>
+
+                {results.map(book => (
+
+                  <div className="card mb-3">
+                    <div className="card-body">
+                      <div className="float-right">
+                        <FormBtn onClick={handleViewSubmit}>
+                          View
+                        </FormBtn>
+                        <FormBtn onClick={handleSaveSubmit}>
+                          Save
+                        </FormBtn>
+                      </div>
+                      <h5 className="card-title">{book.volumeInfo.title}{(book.volumeInfo.subtitle) ? (': ' + book.volumeInfo.subtitle) : ('')}</h5>
+                      <h6 className="card-subtitle mb-2 text-muted">{book.volumeInfo.authors[0]}</h6>
+                      <img src={book.volumeInfo.imageLinks.smallThumbnail} alt={book.title} className="rounded float-left pr-3" />
+                      <p className="card-text">{book.volumeInfo.description}</p>
+                    </div>
+                  </div>
+
+                ))}
+                {/* book.length: {gbook.length}
+                {(gbook.length) ? (
+                  <List>
+                    {gbook.map(gbook => (
+                      <ListItem key={gbook._id}>
+                        <Link to={"/books/" + gbook._id}>
+                          <strong>
+                            {gbook.title} by {gbook.author}
+                          </strong>
+                        </Link>
+                      </ListItem>
+                    ))}
+                  </List>
+                ) : (
+                    <h3>No Results to Display</h3>
+                  )} */}
+
+              </div>
+            </div>
+          </div>
+        </Col>
+      </Row>
+
     </Container>
   );
 }
-
-
-export default Books;
--- client/src/utils/GBSearch.js
+++ client/src/utils/GBSearch.js
@@ -1,42 +1,12 @@
 import axios from 'axios';
 
-// https://www.google.com/search?tbm=bks&q=1q84
+const gbSearchUrl = 'https://www.googleapis.com/books/v1/volumes?q=';
 
 export default {
-  // Gets all books
-  findBooks: function(id) {
-      console.log(id);
-    //   return axios.get("https://dog.ceo/api/breeds/image/random");
-    //   return axios.get("https://www.google.com/search?tbm=bks&q=1q84");
-      return axios.get("https://www.google.com/search?tbm=bks&q=" + {id});
+  findBooks: function(book) {
+      console.log('this is the "bookId": ' + book);
+      console.log(gbSearchUrl + book);
+      // return axios.get(gbSearchUrl + id.q);
+      return axios.get('https://www.googleapis.com/books/v1/volumes?q=' + book + '&maxResults=10');
   }
-  // Gets the book with the given id
-//   getBook: function(id) {
-    // return axios.get("/api/books/" + id);
-//   },
-  // Deletes the book with the given id
-//   deleteBook: function(id) {
-    // return axios.delete("/api/books/" + id);
-//   },
-  // Saves a book to the database
-//   saveBook: function(bookData) {
-    // return axios.post("/api/books", bookData);
-//   }
 };
-
-
-// import axios from "axios";
-
-// // Export an object containing methods we'll use for accessing the Dog.Ceo API
-
-// export default {
-//   getRandomDog: function() {
-//     return axios.get("https://dog.ceo/api/breeds/image/random");
-//   },
-//   getDogsOfBreed: function(breed) {
-//     return axios.get("https://dog.ceo/api/breed/" + breed + "/images");
-//   },
-//   getBaseBreedsList: function() {
-//     return axios.get("https://dog.ceo/api/breeds/list");
-//   }
-// };
